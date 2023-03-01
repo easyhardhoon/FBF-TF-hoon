4eab34a03b92d9263fa2d19cb4ae37f3622d3cad
easyhardhoon <ghoon0040@naver.com>
2023-03-01 13:06:08 +0000

    add test models

--- make_mnist_original.py
+++ make_mnist_original.py
@@ -26,20 +26,20 @@ from tensorflow.keras.layers import Dense, Input,Conv2D, Dropout, Flatten, MaxPo
 from tensorflow.keras.utils import plot_model
 
 
-
-#NOTE 3-times CONV & dynamic concatenate  MNIST Model by Model class
-
-# ---
+#
+##NOTE 3-times CONV & dynamic concatenate  MNIST Model by Model class
+#
+## ---
 class model_GPU(Model):
     def __init__(self):
         super(model_GPU, self).__init__()
-        self.conv1 = Conv2D(10,(3,3),activation=tf.nn.relu)
+        self.conv1 = Conv2D(10,(3,3),activation=tf.nn.leaky_relu)
         self.pool1 = MaxPooling2D(2,2)
-        self.conv2 = Conv2D(20,(3,3),activation=tf.nn.relu)
+        self.conv2 = Conv2D(20,(3,3),activation=tf.nn.leaky_relu)
         self.pool2 = MaxPooling2D(2,2)
-        self.conv3 = Conv2D(40,(3,3),activation=tf.nn.relu)
+        self.conv3 = Conv2D(40,(3,3),activation=tf.nn.leaky_relu)
         self.flat = Flatten()
-        self.dense1 = Dense(128, activation=tf.nn.relu)
+        self.dense1 = Dense(128, activation=tf.nn.leaky_relu)
         self.dense2 = Dense(10, activation = tf.nn.softmax)
         self.dummy1 = Conv2D(1,(3,3), trainable= False)
         self.dummy2 = Conv2D(1,(3,3), trainable= False)
@@ -87,29 +87,29 @@ myGpuModel.save('saved_model/11')
 
 #--------------------------------------------------------------------------------------
 #NOTE just 3-times CONV MNIST Model by sequential class
-# model = Sequential()
-# model.add(Conv2D(10, kernel_size=(3,3), input_shape=input_shape))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Conv2D(20, kernel_size=(3,3)))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Conv2D(40, kernel_size=(3,3)))
-# model.add(Flatten())
-# model.add(Dense(128, activation=tf.nn.relu))
-# model.add(Dropout(0.2))
-# model.add(Dense(10,activation=tf.nn.softmax))
-# model.summary()
-
-
-# model.compile(optimizer='adam',
-#               loss='sparse_categorical_crossentropy',
-#               metrics=['accuracy'])
-# model.fit(x=x_train,y=y_train, epochs=10)
-
-# test_loss, test_acc = model.evaluate(x_test, y_test)
-
-# print('accuacy :', test_acc)
-
-# model.save('saved_model/new_cpu')
-
+#model = Sequential()
+#model.add(Conv2D(10, kernel_size=(3,3), input_shape=input_shape))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+#model.add(Conv2D(20, kernel_size=(3,3)))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+#model.add(Conv2D(40, kernel_size=(3,3)))
+#model.add(Flatten())
+#model.add(Dense(128, activation=tf.nn.relu))
+#model.add(Dropout(0.2))
+#model.add(Dense(10,activation=tf.nn.softmax))
+#model.summary()
+#
+#
+#model.compile(optimizer='adam',
+#              loss='sparse_categorical_crossentropy',
+#              metrics=['accuracy'])
+#model.fit(x=x_train,y=y_train, epochs=10)
+#
+#test_loss, test_acc = model.evaluate(x_test, y_test)
+#
+#print('accuacy :', test_acc)
+#
+#model.save('saved_model/new_cpu')
+#
 #--------------------------------------------------------------------------------------
 
---
+++ readme.txt
@@ -0,0 +1,17 @@
+#tflite models
+mnist_1.tflite -> original model 
+mnist_2.tflite -> change hidden layer's activation [relu->leaky_relu]
+mnist_3.tflite -> remove concatenate layer and add dropout layer at that point
+
+#main flow
+1. minimal_cpu -> test minimal example on single unit CPU 
+2. minimal_gpu -> test minimal example on single unit GPU
+3. minimal_cpu_gpu -> test minimal example on multiple unit, CPU & GPU without any data exchange
+4. unit_simple -> test minimal example on multiple unit, CPU & GPU with data exchange
+
+#unit_simple(***)
+1.
+2.
+3.
+4.
+
