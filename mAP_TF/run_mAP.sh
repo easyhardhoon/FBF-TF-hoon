e605b9a2ec330a5f5eac56ecf635edca073f3783
easyhardhoon <ghoon0040@gmail.com>
2023-06-27 04:46:42 +0000

    YOLO parsing : use three vectors in mAP_TF, out of tflite

---
+++ mAP_TF/run_mAP.sh
@@ -0,0 +1,2 @@
+g++ -o mAP mAP.cc
+./mAP
--- unit_simple.cc
+++ unit_simple.cc
@@ -137,11 +137,11 @@ int main(int argc, char* argv[])
 	#endif
 
 	#ifdef yolo
-	// read_image_opencv("data/dog-group.jpg", input);
+	read_image_opencv("data/dog-group.jpg", input);
 	// read_image_opencv("data/dog_horse_person.jpg", input);
 	// read_image_opencv("data/dog.jpg", input);
 	// read_image_opencv("data/car.jpg", input);
-	read_image_opencv("data/cats.jpg", input);
+	// read_image_opencv("data/cats.jpg", input);
 	std::cout << "Loading dog Image \n";
 	#endif
 
