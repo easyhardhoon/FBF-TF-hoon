d70acaccf32654921607de2d417e970d63a9762f
easyhardhoon <ghoon0040@gmail.com>
2023-09-14 05:49:57 +0000

    Implement APP_yolo_with_pycam for RPI

--- unit_simple.cc
+++ unit_simple.cc
@@ -104,48 +104,48 @@ void read_image_opencv(string filename, vector<cv::Mat>& input){
 }
 #endif
 
-void YOLO_parsing(std::vector<tflite::YOLO_Parser::BoundingBox>& result_boxes, int fnum, std::map<int, std::string>& labelDict)
-  {
-	std::string filename = "../mAP_TF/input/detection-results/" + std::to_string(fnum) + ".txt";
-	std::ofstream outFile(filename);
-	if (!outFile.is_open()) {
-        std::cerr << "Error: Unable to open file " << filename << std::endl;
-        return;
-    }
-	for (int i=0; i <result_boxes.size(); i++) { 
-		auto object_name = labelDict[result_boxes[i].class_id];
-		auto left = result_boxes[i].left;
-		auto top = result_boxes[i].top;
-		auto right = result_boxes[i].right;
-		auto bottom = result_boxes[i].bottom;
-		auto cls_data = result_boxes[i].score;
-		outFile << object_name << " " <<  cls_data << " ";
-		outFile << left << " " << top << " " << right << " " << bottom; 
-		outFile << std::endl;
-	}
-	outFile.close();
-  }
+// void YOLO_parsing(std::vector<tflite::YOLO_Parser::BoundingBox>& result_boxes, int fnum, std::map<int, std::string>& labelDict)
+//   {
+// 	std::string filename = "../mAP_TF/input/detection-results/" + std::to_string(fnum) + ".txt";
+// 	std::ofstream outFile(filename);
+// 	if (!outFile.is_open()) {
+//         std::cerr << "Error: Unable to open file " << filename << std::endl;
+//         return;
+//     }
+// 	for (int i=0; i <result_boxes.size(); i++) { 
+// 		auto object_name = labelDict[result_boxes[i].class_id];
+// 		auto left = result_boxes[i].left;
+// 		auto top = result_boxes[i].top;
+// 		auto right = result_boxes[i].right;
+// 		auto bottom = result_boxes[i].bottom;
+// 		auto cls_data = result_boxes[i].score;
+// 		outFile << object_name << " " <<  cls_data << " ";
+// 		outFile << left << " " << top << " " << right << " " << bottom; 
+// 		outFile << std::endl;
+// 	}
+// 	outFile.close();
+//   }
 
-void visualize_with_labels(cv::Mat& image, const std::vector<tflite::YOLO_Parser::BoundingBox>& bboxes, std::map<int, std::string>& labelDict) {
-    for (const tflite::YOLO_Parser::BoundingBox& bbox : bboxes) {
-        int x1 = bbox.left;
-        int y1 = bbox.top;
-        int x2 = bbox.right;
-        int y2 = bbox.bottom;
-        cv::RNG rng(bbox.class_id);
-        cv::Scalar color(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-        int label_x = x1;
-        int label_y = y1 - 20;
+// void visualize_with_labels(cv::Mat& image, const std::vector<tflite::YOLO_Parser::BoundingBox>& bboxes, std::map<int, std::string>& labelDict) {
+//     for (const tflite::YOLO_Parser::BoundingBox& bbox : bboxes) {
+//         int x1 = bbox.left;
+//         int y1 = bbox.top;
+//         int x2 = bbox.right;
+//         int y2 = bbox.bottom;
+//         cv::RNG rng(bbox.class_id);
+//         cv::Scalar color(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+//         int label_x = x1;
+//         int label_y = y1 - 20;
 
-        cv::rectangle(image, cv::Point(x1, y1), cv::Point(x2, y2), color, 3);
-        std::string object_name = labelDict[bbox.class_id];
-        float confidence_score = bbox.score;
-        std::string label = object_name + ": " + std::to_string(confidence_score);
-        cv::Size text_size = cv::getTextSize(label, cv::FONT_HERSHEY_SIMPLEX, 0.6, 2, nullptr);
-        cv::rectangle(image, cv::Point(x1, label_y - text_size.height), cv::Point(x1 + text_size.width, label_y + 5), color, -1);
-        cv::putText(image, label, cv::Point(x1, label_y), cv::FONT_HERSHEY_SIMPLEX, 0.6, cv::Scalar(255, 255, 255), 2);
-    }
-}
+//         cv::rectangle(image, cv::Point(x1, y1), cv::Point(x2, y2), color, 3);
+//         std::string object_name = labelDict[bbox.class_id];
+//         float confidence_score = bbox.score;
+//         std::string label = object_name + ": " + std::to_string(confidence_score);
+//         cv::Size text_size = cv::getTextSize(label, cv::FONT_HERSHEY_SIMPLEX, 0.6, 2, nullptr);
+//         cv::rectangle(image, cv::Point(x1, label_y - text_size.height), cv::Point(x1 + text_size.width, label_y + 5), color, -1);
+//         cv::putText(image, label, cv::Point(x1, label_y), cv::FONT_HERSHEY_SIMPLEX, 0.6, cv::Scalar(255, 255, 255), 2);
+//     }
+// }
 
 int main(int argc, char* argv[])
 {
@@ -206,7 +206,7 @@ int main(int argc, char* argv[])
 	int max_delegated_partition_num = Max_Delegated_Partitions_Num;
 	int test_number  = combination(Partition_Num, Max_Delegated_Partitions_Num);
 	if(!bUseTwoModel){
-		test_number = 300;  // FULL 330
+		// test_number = 1;  // FULL 330
 		int fnum = 0;	    // FULL	
 		for (int loop_num=0; loop_num<test_number; loop_num++)
 		{
@@ -225,23 +225,23 @@ int main(int argc, char* argv[])
 			}
 			printf("%d loop End.....\n", loop_num);
   			////////////////////////////////////////////////////////////////////////////////////////////
-			std::vector<tflite::YOLO_Parser::BoundingBox> bboxes = tflite::YOLO_Parser::result_boxes;
-			YOLO_parsing(bboxes, fnum, labelDict);
-			// visualize
-			std::string window_name = std::to_string(fnum) + "'s parsed image";
-			cv::namedWindow(window_name, cv::WINDOW_NORMAL);
-			cv::Mat image = cv::imread(filename, cv::IMREAD_COLOR);
-			cv::resize(image, image, cv::Size(416,416)); 
-    		if (!image.empty()) {
-    		    visualize_with_labels(image, bboxes, labelDict);
-        		cv::imshow(window_name, image);
-			}
-			else {
-    			std::cerr << "Error: Unable to load the image: " << filename << std::endl;
-			}
+			// std::vector<tflite::YOLO_Parser::BoundingBox> bboxes = tflite::YOLO_Parser::result_boxes;
+			// YOLO_parsing(bboxes, fnum, labelDict);
+			// // visualize
+			// std::string window_name = std::to_string(fnum) + "'s parsed image";
+			// cv::namedWindow(window_name, cv::WINDOW_NORMAL);
+			// cv::Mat image = cv::imread(filename, cv::IMREAD_COLOR);
+			// cv::resize(image, image, cv::Size(416,416)); 
+    		// if (!image.empty()) {
+    		//     visualize_with_labels(image, bboxes, labelDict);
+        	// 	cv::imshow(window_name, image);
+			// }
+			// else {
+    		// 	std::cerr << "Error: Unable to load the image: " << filename << std::endl;
+			// }
     	}
-		cv::waitKey(0);
-		cv::destroyAllWindows();
+		// cv::waitKey(0);
+		// cv::destroyAllWindows();
 		////////////////////////////////////////////////////////////////////////////////////////////
 
 	}
